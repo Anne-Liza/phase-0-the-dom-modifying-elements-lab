1d46f03dbe717cd2868e6f7f007ca04c25ea4fc3
Anne <annewangui210@gmail.com>
2024-07-12 18:13:39 +0300

    phase-0-the-dom-modifying-elements-lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+// remove the main element
+const currentMain = document.getElementById('main');
+if (currentMain){
+currentMain.remove();
+};
+
+//adding data to h1
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'Anne Wangui is the champion';
+
+document.body.append(newHeader);
